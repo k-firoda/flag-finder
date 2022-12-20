372b642b951390455398a9477cb41460713163ed
=k-firoda <=>
2022-12-20 09:24:43 +0530

    adding some css

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -132,9 +132,10 @@ btn.addEventListener('click', () => {
 const getCountryData = async function (country) {
   const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
   const countries = await res.json();
+  let name = countries[0].name.common.toLowerCase();
   const data = countries[0];
-  console.log(data);
-  const html = `
+  if (name === country) {
+    const html = `
         <article class="country">
             <img class="" src="${data.flags.png}" />
             <div class="mt-2">
@@ -146,7 +147,8 @@ const getCountryData = async function (country) {
             <p class="flex">Coat of Arms :<img class="w-6 m-2" src="${data.coatOfArms.png}" /></p>
         </article>
      `;
-  imgDiv.insertAdjacentHTML('beforeend', html);
+    imgDiv.insertAdjacentHTML('beforeend', html);
+  }
 };
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -173,7 +175,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61288" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60881" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
